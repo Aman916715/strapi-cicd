6cbc2423ee536b71a1f74ec9b4c6f694204d9612
Aman916715 <amangupta9076@gmail.com>
2025-09-17 21:20:55 +0530

    Add docker-compose file and update in .env

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,9 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+export default ({ env }: { env: any }) => ({
+  host: '0.0.0.0',
   port: env.int('PORT', 1337),
+  url: env('APP_URL', 'http://localhost'),
   app: {
-    keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']),
+    keys: env.array('APP_KEYS'),
   },
+  proxy: true,
 });
